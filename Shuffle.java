507549bb19661bfde0080b7e676bd0802eabef1c
ronisha-shigdel <ronishashigdel@gmail.com>
2020-11-12 21:13:23 +0545

    changed variables

--- Shuffle.java
+++ Shuffle.java
@@ -2,16 +2,16 @@ public class Shuffle{
 	public static void main(String[] args){
 		int x = 3;
 		if (x>2){
-			System.out.print("a");
+			System.out.print("apple");
 		}
 		while (x>0){
 			x = x-1;
 			System.out.print ("-");
 			if (x==2){
-				System.out.print ("b c");
+				System.out.print ("ball cat");
 			}
 			if (x==1){
-				System.out.print ("d");
+				System.out.print ("dog");
 				x = x-1;
 			}
 		}
